0cc591d63d4558021c6a4b1459f59a2962c084c8
oleast <oleast@stud.ntnu.no>
2017-09-12 12:34:31 +0200

    Adding nunjucks

--- server/app.js
+++ server/app.js
@@ -1,8 +1,19 @@
 const express = require('express')
+const nunjucks = require('nunjucks')
 
 const HOST = '0.0.0.0'
 const PORT = 8000
 
 let app = express()
+
+nunjucks.configure('server/views', {
+    autoescape: true,
+    express: app,
+    watch: true,
+})
+
+app.get('/', (req, res) => {
+    res.render('index.html')
+})
 
 let server = app.listen(PORT, HOST, () => console.log('Project server running on: ' + HOST + ':' + PORT))
