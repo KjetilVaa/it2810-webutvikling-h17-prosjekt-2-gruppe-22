b86e0073bbe5e42291d76ad8b9d8249fd777f433
frideska <frideskarz28@gmail.com>
2017-09-17 19:37:05 +0200

    Two alternatives for navbar

--- server/app.js
+++ server/app.js
@@ -13,10 +13,10 @@ nunjucks.configure('server/views', {
 })
 
 const anchors = [
-    {button: "pictures-anchor-button", name: "Water", link: "/water"},
-    {button: "description-anchor-button", name: "Forest", link: "/forest"},
-    {button: "top-anchor-button", name: "Mountain", link: "/mountain"},
-    {button: "top-anchor-button", name: "City", link: "/city"}
+    {button: "pictures-anchor-button", Source: "/public/media/water.png", name: "Water", link: "/water"},
+    {button: "top-anchor-button", Source: "/public/media/mountain.png", name: "Mountain", link: "/mountain"},
+    {button: "description-anchor-button", Source: "/public/media/forest.png", name: "Forest", link: "/forest"},
+    {button: "top-anchor-button", Source: "/public/media/city.png", name: "City", link: "/city"}
 ]
 
 app.use('/public', express.static('./public'))
